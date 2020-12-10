1cc9ed500b6b8db502ad3feec136866b079f7a9d
LuisAndresRojasMurillo <lrojasmurillo7@gmail.com>
2020-12-10 17:04:55 -0600

    tablero

--- Proyecto/Tablero.java
+++ Proyecto/Tablero.java
@@ -1,8 +1,48 @@
-package application;
+import java.util.ArrayList;
 
 public class Tablero {
 	
 	private Coordenada casillas[];
 	private Barco barcos[];
+	
+	
+	public Tablero() {
+		super();
+		this.casillas = new Coordenada[100];
+		this.barcos = new Barco[9];
+	}
 
+	
+	public Coordenada[] getCasillas() {
+		return casillas;
+	}
+
+	public void setCasillas(Coordenada[] casillas) {
+		this.casillas = casillas;
+	}
+
+	public Barco[] getBarcos() {
+		return barcos;
+	}
+
+	public void setBarcos(Barco[] barcos) {
+		this.barcos = barcos;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
