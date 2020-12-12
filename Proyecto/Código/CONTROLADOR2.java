c20332ba284268facb1113f0fe61f75cf6110591
Beto Zumbado <zumbado.abarca.alberto@gmail.com>
2020-12-12 16:58:25 -0600

    Segunda version interfazes

--- Proyecto/Código/CONTROLADOR2.java
+++ Proyecto/Código/CONTROLADOR2.java
@@ -4,14 +4,27 @@ import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.MenuButton;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class CONTROLADOR2 implements Initializable {
+	@FXML
+	private ComboBox<ImageView> botonBarco;
 	
+	@FXML
+	private ComboBox<String> EjeX;
+	
+	@FXML
+	private ComboBox<String> EjeY;
+	
+	@FXML
+	private ComboBox<String> posicion;
 	
 	@FXML
 	private ImageView fondo;
@@ -48,7 +61,11 @@ public class CONTROLADOR2 implements Initializable {
 
 	@FXML
 	public void ponerImagen() {
-		tablero.add(portaAviones, 0, 0);
+		ImageView barcoSeleccionado = botonBarco.getSelectionModel().getSelectedItem();
+		int ejex = Integer.parseInt(EjeX.getSelectionModel().getSelectedItem().toString());
+		int ejey = Integer.parseInt(EjeY.getSelectionModel().getSelectedItem().toString());
+		String orientacion = posicion.getSelectionModel().getSelectedItem().toString();
+		
 	}
 	
 	
@@ -67,6 +84,18 @@ public class CONTROLADOR2 implements Initializable {
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		
+		ObservableList<ImageView> barcos = FXCollections.observableArrayList(portaAviones,submarino1,submarino2,
+				submarino3,destructor1,destructro2,desctructor3,fragata1,fragata2);
+		botonBarco.setItems(barcos);
+		
+		ObservableList<String> ejes = FXCollections.observableArrayList("1", "2", "3","4", "5", "6", "7", "8", "9","10");
+		EjeX.setItems(ejes);
+		EjeY.setItems(ejes);
+		
+
+
+		ObservableList<String> orientacion = FXCollections.observableArrayList("Horizontal", "Vertical");
+		posicion.setItems(orientacion);
 	}
 	
 	
