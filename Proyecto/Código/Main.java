09667c19d1c459d544f918ffdeed608a9b532590
LuisAndresRojasMurillo <lrojasmurillo7@gmail.com>
2020-12-16 18:42:07 -0600

    juego terminado2

---
+++ Proyecto/Código/Main.java
@@ -0,0 +1,25 @@
+package application;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+    	
+        Parent root = FXMLLoader.load(getClass().getResource("INICIO.fxml"));
+        primaryStage.setTitle("Battleship");
+        
+        Scene scene = new Scene(root, 750, 600);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+	
+}
